51ca6f9977426c9f42f234153a65ed4146626091
Yoann JOLY <yoannjly@gmail.com>
2022-11-13 17:03:26 +0100

    Commit

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ function myFunction() {
         }
     });
 }
+
+/* Sélection des éléments HTML */
+let link = document.getElementById('burger-button')
+let burger = document.querySelector('header nav')
+
+/* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
+link.addEventListener('click', function(e) {
+    e.preventDefault()
+    burger.classList.toggle('open')
+})
